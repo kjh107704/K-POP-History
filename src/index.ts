c77adf4b34834f267dd10ad4f5ddbddc33cd9604
Kim Juhui <kjh107704@gmail.com>
2020-04-11 23:18:37 +0900

    use express.json to parse incoming requests with JSON payloads

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './router/index';
 
 const app = express();
+app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
     res.send('hello world');
